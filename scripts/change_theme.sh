0941c360122cfb4f43a1f586cac55b5a55b473aa
Apple <Apple@server.fake>
2013-11-04 09:28:35 +0000

    made change_script more general over my laptops

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -7,23 +7,23 @@ setupnew () {
 }
 
 # Define the rc locations
-SHIFTYRC=/home/apple/.config/awesome/the_rcs/shifty.lua
-HOLORC=/home/apple/.config/awesome/the_rcs/holo.lua
-DUSTRC=/home/apple/.config/awesome/the_rcs/dust.lua
-MULTICOLORRC=/home/apple/.config/awesome/the_rcs/multicolor.lua
-STEAMBURNRC=/home/apple/.config/awesome/the_rcs/steamburn.lua
-MUTEDRC=/home/apple/.config/awesome/the_rcs/muted.lua
-MAINRC=/home/apple/.config/awesome/the_rcs/main.lua
+SHIFTYRC=~/.config/awesome/the_rcs/shifty.lua
+HOLORC=~/.config/awesome/the_rcs/holo.lua
+DUSTRC=~/.config/awesome/the_rcs/dust.lua
+MULTICOLORRC=~/.config/awesome/the_rcs/multicolor.lua
+STEAMBURNRC=~/.config/awesome/the_rcs/steamburn.lua
+MUTEDRC=~/.config/awesome/the_rcs/muted.lua
+MAINRC=~/.config/awesome/the_rcs/main.lua
 
 
 # Define the xresources colour locations
-SHIFTYCOLS=/home/apple/xresources/awesome_personal_resources
-HOLOCOLS=/home/apple/xresources/holo
-DUSTCOLS=/home/apple/xresources/vinyl
-MULTICOLORCOLS=/home/apple/xresources/dremora
-STEAMBURNCOLS=/home/apple/xresources/steamburn
-MUTEDCOLS=/home/apple/xresources/awesome_muted_resources
-MAINCOLS=/home/apple/xresources/awesome_personal_resources
+SHIFTYCOLS=~/xresources/awesome_personal_resources
+HOLOCOLS=~/xresources/holo
+DUSTCOLS=~/xresources/vinyl
+MULTICOLORCOLS=~/xresources/dremora
+STEAMBURNCOLS=~/xresources/steamburn
+MUTEDCOLS=~/xresources/awesome_muted_resources
+MAINCOLS=~/xresources/awesome_personal_resources
 
 case $1 in
     "shifty")  
